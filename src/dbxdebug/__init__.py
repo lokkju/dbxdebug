f0399b5db0e3d709828769eb40aec19e542fa239
Loki Coyote <lokkju@gmail.com>
2025-12-17 02:15:43 -0800

    Export all library functions from package root
    Add comprehensive exports to __init__.py:
    - Video: dos_video_to_html, analyze_dos_video_colors, VGA constants
    - Capture I/O: load_capture, save_capture, get_capture_path
    - Keyboard: expanded helpers (key_list, digit_key, number_keys)
    - Key constants: TAB, BACKSPACE, SPACE, DELETE, ALT_F4, etc.
    - Utilities: char_to_qcode, char_needs_shift, format_attribute_info

--- src/dbxdebug/__init__.py
+++ src/dbxdebug/__init__.py
@@ -10,20 +10,60 @@ Provides:
 
 from .gdb import GDBClient
 from .qmp import QMPClient, QMPError
-from .video import DOSVideoTools, decode_vga_attribute
-from .dbx_kbd import DBX_KEY, DBX_KEY_TO_QCODE, QCODE_TO_DBX_KEY
+from .video import (
+    DOSVideoTools,
+    decode_vga_attribute,
+    format_attribute_info,
+    DOS_VIDEO_PAGE_ONE,
+    DOS_VIDEO_PAGE_TWO,
+    DOS_VIDEO_MEMORY_SIZE,
+    BDA_TIMER_TICK,
+    TIMER_FREQUENCY,
+)
+from .dbx_kbd import (
+    DBX_KEY,
+    DBX_KEY_TO_QCODE,
+    QCODE_TO_DBX_KEY,
+    dbx_key_to_qcode,
+    qcode_to_dbx_key,
+    char_to_qcode,
+    char_needs_shift,
+)
 from .utils import parse_x86_address, hexdump
 from .keyboard import (
+    key_list,
     ctrl_key,
     alt_key,
     shift_key,
     ctrl_alt_key,
+    ctrl_shift_key,
     function_key,
+    digit_key,
+    number_keys,
     ENTER,
     ESCAPE,
+    TAB,
+    BACKSPACE,
+    SPACE,
+    DELETE,
     CTRL_C,
+    CTRL_V,
+    CTRL_X,
+    CTRL_Z,
+    CTRL_A,
+    CTRL_S,
     CTRL_ALT_DEL,
+    ALT_F4,
+    ALT_TAB,
 )
+from .html import (
+    dos_video_to_html,
+    save_dos_video_html,
+    analyze_dos_video_colors,
+    VGA_COLORS,
+    VGA_COLOR_NAMES,
+)
+from .capture_io import load_capture, save_capture, get_capture_path
 
 __version__ = "0.1.0"
 
@@ -32,23 +72,59 @@ __all__ = [
     "GDBClient",
     "QMPClient",
     "QMPError",
-    # Video
+    # Video tools
     "DOSVideoTools",
     "decode_vga_attribute",
-    # Keys
+    "format_attribute_info",
+    "dos_video_to_html",
+    "save_dos_video_html",
+    "analyze_dos_video_colors",
+    # Video constants
+    "DOS_VIDEO_PAGE_ONE",
+    "DOS_VIDEO_PAGE_TWO",
+    "DOS_VIDEO_MEMORY_SIZE",
+    "BDA_TIMER_TICK",
+    "TIMER_FREQUENCY",
+    "VGA_COLORS",
+    "VGA_COLOR_NAMES",
+    # Key codes
     "DBX_KEY",
     "DBX_KEY_TO_QCODE",
     "QCODE_TO_DBX_KEY",
+    "dbx_key_to_qcode",
+    "qcode_to_dbx_key",
+    "char_to_qcode",
+    "char_needs_shift",
     # Keyboard helpers
+    "key_list",
     "ctrl_key",
     "alt_key",
     "shift_key",
     "ctrl_alt_key",
+    "ctrl_shift_key",
     "function_key",
+    "digit_key",
+    "number_keys",
+    # Common key constants
     "ENTER",
     "ESCAPE",
+    "TAB",
+    "BACKSPACE",
+    "SPACE",
+    "DELETE",
     "CTRL_C",
+    "CTRL_V",
+    "CTRL_X",
+    "CTRL_Z",
+    "CTRL_A",
+    "CTRL_S",
     "CTRL_ALT_DEL",
+    "ALT_F4",
+    "ALT_TAB",
+    # Capture I/O
+    "load_capture",
+    "save_capture",
+    "get_capture_path",
     # Utilities
     "parse_x86_address",
     "hexdump",
